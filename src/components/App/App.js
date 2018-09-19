bd7a8eda29f549e4de902aa5ba5d75730b59d232
Maks Egorov <emax@makeit-da.ru>
2018-09-19 10:59:51 +0300

    edited App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="description">Миру-мир, студентам - beer.</p>
+        <p className="description">Создание первой компоненты App.</p>
       </div>
     );
   }
